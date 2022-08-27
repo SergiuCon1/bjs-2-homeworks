52962dc5556f4314eea6a6574bd3c26fa471c2d1
Sergiu Contevici <sergiucontevici@gmail.com>
2022-08-28 00:28:14 +0300

    resolve 1,2,3 tasks

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,61 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
 
-  // Ваш код
+  for (let array of arr) {
+    if (array < min) {
+      min = array;
+    } else if (array > max) {
+      max = array;
+    }
+    sum += array;
+  }
+  
+  avg = sum / arr.length;
 
-  return { min: min, max: max, avg: avg };
+  return { min: min, max: max, avg: Number(avg.toFixed(2)) };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let array of arr) {
+      sum += array;
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    sumArr = func(arrOfArr[i]);
+    if (sumArr > max) {
+      max = sumArr;
+  }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
+  let min = arr[0];
+  let max = arr[0];
+  for (let array of arr) {
+    if (array < min) {
+      min = array;
+    } else if (array > max) { 
+      max = array;
+    }
+  }
+  let diffenence = max - min;
+
+  return Math.abs(diffenence);
   // Ваш код
 }
