0998e43e130f29f7092b21e834569ab6beb73c7d
Sergiu Contevici <sergiucontevici@gmail.com>
2022-08-30 09:30:00 +0300

    fix task 1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,12 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  if (arr1.length === arr2.length) {
-    result = arr1.every((item, index) => item === arr2[index]);
-  } else {
-    result = false;
-  }
 
-  return result; // boolean
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]); // boolean
+  
 }
 
 function advancedFilter(arr) {
