02600ad7d522196fa0f137b7e4fec5605350fc32
Sergiu Contevici <sergiucontevici@gmail.com>
2022-09-13 09:46:02 +0300

    Home work for lesson "Object"

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks = marks;
+}
+
+Student.prototype.getAverage = function () {
+  let sumMarks = 0;
+  for (let mark of this.marks) {
+    sumMarks += mark;
+  }
+  return sumMarks / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete Student.subject;
+  delete Student.marks;
+  this.excluded = reason;
+}
 // ваш код для остальных методов
